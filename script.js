5c74be913c40045858c1d6973df312df6a85be09
AV-room <AVroom.github@gmail.com>
2017-11-30 22:55:26 +1100

    v10 - refactoring change and delete

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// V9: ESCAPE FROM THE CONSOLE (DOM MANIPULATION)
-// There should be an li element for every todo
-// Each li element should contain todoText
-// Each li element should show completed indicator
+// V10: REFACTORING DELETE AND TOGGLE COMPLETED
+// There should be a way to create delete buttons
+// There should be a delete button for each todo
+// Each li should have an id that has the todo position
+// Delete buttons should have access to the todo id
+// Clicking delete should update todoList.todos and the DOM
 
 //now only contains todo list data and methods to change that data
 var todoList = {
-  todos: [{
-          todoText: 'brush hair',
-          completed: false
-      }, 
-      {
-          todoText: 'eat waffles',
-          completed: false
-      },
-      {
-          todoText: 'feed giraffe',
-          completed: false
-      }],
+  todos: [],
   addTodo: function(newTodo) {
     this.todos.push({
       todoText: newTodo,
@@ -65,10 +56,6 @@ var handlers = {
     displayTodos: function() {
         view.displayTodos();
     },
-    toggleAll: function() {
-        todoList.toggleAll();
-        view.displayTodos();
-    },
     addTodo: function() {
         var addTodoInput = document.getElementById('addText');
         
@@ -76,6 +63,11 @@ var handlers = {
         
         addTodoInput.value = '';
         
+        view.displayTodos();
+    },
+    deleteTodo: function(pos) {
+        todoList.deleteTodo(pos);
+       
         view.displayTodos();
     },
     changeTodo: function() {
@@ -89,22 +81,13 @@ var handlers = {
         
         view.displayTodos();
     },
-    deleteTodo: function() {
-        var deletePosInput = document.getElementById('deletePos');
-        
-        todoList.deleteTodo(deletePosInput.valueAsNumber);
-        
-        deletePosInput.value = '';
+    toggleCompleted: function(pos) {
+        todoList.toggleCompleted(pos);
         
         view.displayTodos();
     },
-    toggleCompleted: function() {
-        var toggleCompletedPosInput = document.getElementById('toggleCompletedPos');
-        
-        todoList.toggleCompleted(toggleCompletedPosInput.valueAsNumber);
-        
-        toggleCompletedPosInput.value = '';
-        
+    toggleAll: function() {
+        todoList.toggleAll();
         view.displayTodos();
     }
 };
@@ -112,22 +95,80 @@ var handlers = {
 // only shows people what todo list looks like
 var view = {
   displayTodos: function() {
-    
+      
     //select ul element and reset to empty
     var ulElement = document.querySelector('ul');
     ulElement.innerHTML = '';
       
     //create an li element for each item in todo list and append to ul element
     for(var i=0; i<todoList.todos.length; i++) {
+        
+        //create and configure li element
         var liElement = document.createElement('li');
+        
+        liElement.id = i;
+        
         var currentTodo = todoList.todos[i]; 
         liElement.textContent = createCompletedSymbol(currentTodo.completed) + ' ' + currentTodo.todoText;
+        
+        //append buttons
+        liElement.appendChild(this.createToggleCompletedButton());
+        liElement.appendChild(this.createDeleteButton());
+        
+        //add the li to the parent ul element
         ulElement.appendChild(liElement);
     }
       
-  }  
+  },
+  createDeleteButton: function() {
+    //create button element 
+    var deleteButton = document.createElement('button');
+    
+    //assign button text content + classname
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+      
+    return deleteButton;
+  },
+  createToggleCompletedButton: function() {
+    var toggleCompletedButton = document.createElement('button');
+    toggleCompletedButton.textContent = 'Toggle Completed';
+    toggleCompletedButton.className = 'toggleCompletedButton';
+    return toggleCompletedButton;
+  },
+  setUpEventListeners: function() {
+    
+    //add click event listener to ul
+    var ulElement = document.querySelector('ul'); //this is ok because we only have one ul on the page
+    
+    //check to see if element that was clicked was a delete button
+    //if so, get the id of the associated li element
+    //and delete that item from the todoList and update the DOM
+    ulElement.addEventListener('click', function(e) {
+        var elementClicked = e.target;
+        
+        if(elementClicked.className === 'deleteButton'){
+            handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+        }
+        
+        if(elementClicked.className === 'toggleCompletedButton'){
+            handlers.toggleCompleted(parseInt(elementClicked.parentNode.id));
+        }
+    })
+  }
 };
 
+// helper function
 function createCompletedSymbol(completed) {
     return completed ? '(x)' : '( )';
 }
+
+
+view.setUpEventListeners();
+
+
+/*var testUl = document.getElementById('testUl');
+console.log(testUl);
+testUl.addEventListener('click', function(e){
+   console.log('clicked on ' + e.target + ' id: ' + e.target.id); 
+});*/
